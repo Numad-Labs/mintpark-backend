301d9633c4c39b5f23c1f90fa91d2cdce15ea8a2
dgduksict <B211910809@must.edu.mn>
2024-10-17 14:02:48 +0800

    feeRate helper added

---
+++ blockchain/utxo/calculateRequiredAmount.ts
@@ -0,0 +1,71 @@
+import { WITNESS_SCALE_FACTOR } from "./constants";
+
+export function calculateInscriptionSize(
+  data: Buffer,
+  contentType: string
+): number {
+  const mimeTypeSize = contentType.length;
+  const dataSize = data.length;
+  const maxChunkSize = 520;
+
+  let size = 2 + mimeTypeSize + 1; // OP_FALSE OP_IF + mime type + OP_0
+
+  for (let i = 0; i < dataSize; i += maxChunkSize) {
+    size += Math.min(maxChunkSize, dataSize - i) + 1; // Add 1 for the push opcode
+  }
+
+  size += 1; // OP_ENDIF
+  return Math.ceil(size / 4);
+}
+
+export function getEstimatedFee(
+  fileSize: number[],
+  mimeTypeByteSize: number[],
+  serviceFee: number,
+  feeRate: number,
+  price: number = 0
+) {
+  const dustLimit = 546;
+  let amount = 0;
+  if (fileSize.length !== mimeTypeByteSize.length) {
+    throw new Error("File size and mime type size must be the same length.");
+  }
+  for (let i = 0; i < fileSize.length; i++) {
+    let commitSize = 0;
+    if (price > 0) {
+      commitSize = Math.ceil(10 + 58 * 2 + 43 * 3);
+    } else {
+      commitSize = Math.ceil(10 + 58 * 1 + 43 * 3);
+    }
+
+    const inscriptionSize =
+      (33 +
+        1 +
+        1 +
+        1 +
+        4 +
+        2 +
+        1 +
+        mimeTypeByteSize[i] +
+        2 +
+        Math.ceil(fileSize[i] / 520) +
+        fileSize[i] +
+        1 +
+        10) /
+      WITNESS_SCALE_FACTOR;
+    const revealSize = Math.ceil(10 + 58 * 1 + 43 * 1 + inscriptionSize);
+    const commitFee = commitSize * feeRate;
+    const revealFee = revealSize * feeRate;
+    amount += commitFee + revealFee + dustLimit + serviceFee;
+  }
+  amount += price;
+  return {
+    estimatedFee: {
+      feeRate: feeRate,
+      price: price,
+      networkFee: amount - price - serviceFee * fileSize.length,
+      serviceFee: serviceFee * fileSize.length,
+      totalAmount: amount,
+    },
+  };
+}
