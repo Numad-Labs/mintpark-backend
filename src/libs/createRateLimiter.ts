d7677b7c1eb445e832a0446db30673c411e39860
gombochirr <gombochir.dev@gmail.com>
2024-11-04 11:26:14 +0800

    disable ratelimiter

--- src/libs/createRateLimiter.ts
+++ src/libs/createRateLimiter.ts
@@ -19,7 +19,7 @@ export function createRateLimiter(options: RateLimiterOptions) {
     res: Response,
     next: NextFunction
   ) {
-    if (config.NODE_ENV === "test") {
+    if (config.NODE_ENV !== "") {
       return next();
     }
 
