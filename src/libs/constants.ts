4438f0c67fb2523ffb839c47f177152f9072d01f
gombochirr <gombochir.dev@gmail.com>
2025-03-12 12:14:15 +0800

    increase FILE_SIZE_LIMIT, formDataSizeLimit

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -2,7 +2,7 @@ export const intervalMap: { [key: string]: number } = {
   "1h": 60 * 60 * 1000,
   "24h": 24 * 60 * 60 * 1000,
   "7d": 7 * 24 * 60 * 60 * 1000,
-  "30d": 30 * 24 * 60 * 60 * 1000,
+  "30d": 30 * 24 * 60 * 60 * 1000
 };
 
 export const MINIMUM_LISTING_SERVICE_FEE = 10000;
@@ -12,7 +12,7 @@ export const BADGE_BATCH_SIZE = 25;
 
 export const ON_HOLD_MINUTES = 3;
 
-export const FILE_SIZE_LIMIT = 37748736; //36MB
+export const FILE_SIZE_LIMIT = 104857600; //100MB
 export const FILE_COUNT_LIMIT = 10;
 
 export const REDIS_KEYS = {
@@ -20,10 +20,10 @@ export const REDIS_KEYS = {
   COLLECTION_PREFIX: "upload:collection:",
   getCollectionKey: (collectionId: string) =>
     `upload:collection:${collectionId}`,
-  BITCOIN_FEE_RATES: "bitcoin_mempool_feerate",
+  BITCOIN_FEE_RATES: "bitcoin_mempool_feerate"
 };
 
 export const sizeLimitConstants = {
   jsonSizeLimit: 5 * 1024 * 1024, //5MB
-  formDataSizeLimit: "40mb",
+  formDataSizeLimit: "40mb"
 };
